544cbc3ca800ea3108e1f8c2a491a57f1aee29b2
Dancan <ngagadancan2003@gmail.com>
2024-03-21 09:58:48 +0300

    first commit

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from api.views import project_list, project_detail, task_list, task_detail
+# from api.views import project_list, project_detail, task_list, task_detail
+from api.views import ProjectListView, ProjectDetailView, TaskListView, TaskDetailView
 
 urlpatterns = [
-    path('project', project_list, name='project-list'),
-    path('project/<int:id>/', project_detail, name='project-detail'),
-    path('task', task_list, name='task-list'),
-    path('task/<int:id>/', task_list, name='task-list'),
+    path('project', ProjectListView.as_view(), name='project-list'),
+    path('project/<int:id>/', ProjectDetailView.as_view(), name='project-detail'),
+    path('task', TaskListView.as_view(), name='task-list'),
+    path('task/<int:id>/', TaskDetailView.as_view(), name='task-list'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,27 +3,43 @@ from api.serializers import ProjectSerializer, TaskSerializer
 from project.models import Project
 from task.models import Task
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
-@api_view(['GET'])
-def project_list(request):
-    projects = Project.objects.all()
-    serializer = ProjectSerializer(projects, many=True)
-    return Response(serializer.data, status=200)
+class ProjectListView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    lookup_field = 'id'
+class TaskListView(generics.RetrieveUpdateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    lookup_field = 'id'
 
-@api_view(['GET'])
-def project_detail(request,id):
-    projects = Project.objects.get(pk=id)
-    serializer = ProjectSerializer(projects)
-    return Response(serializer.data, status=200)
+# @api_view(['GET'])
+# def project_list(request):
+#     projects = Project.objects.all()
+#     serializer = ProjectSerializer(projects, many=True)
+#     return Response(serializer.data, status=200)
 
-@api_view(['GET'])
-def task_list(request):
-    tasks = Task.objects.all()
-    serializer = TaskSerializer(tasks, many=True)
-    return Response(serializer.data, status=200)
+# @api_view(['GET'])
+# def project_detail(request,id):
+#     projects = Project.objects.get(pk=id)
+#     serializer = ProjectSerializer(projects)
+#     return Response(serializer.data, status=200)
 
-@api_view(['GET'])
-def task_detail(request, id):
-    tasks = Task.objects.get(pk=id)
-    serializer = TaskSerializer(tasks)
-    return Response(serializer.data, status=200)
+# @api_view(['GET'])
+# def task_list(request):
+#     tasks = Task.objects.all()
+#     serializer = TaskSerializer(tasks, many=True)
+#     return Response(serializer.data, status=200)
+
+# @api_view(['GET'])
+# def task_detail(request, id):
+#     tasks = Task.objects.get(pk=id)
+#     serializer = TaskSerializer(tasks)
+#     return Response(serializer.data, status=200)
